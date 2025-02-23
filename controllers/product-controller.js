cb316f2dc67b91adc34e5e2f82c9e1dc91c65e63
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-02-23 19:10:13 +0530

    Add files via upload

---
+++ controllers/product-controller.js
@@ -0,0 +1,8 @@
+// it is an obje bec . kga h
+// to import indexController.homeController  bec it is an objext
+// so we can directly get by putting {}
+ 
+module.exports.productController = (req, res) => {
+    res.send("Hey from Product Controller");
+  };
+  
---
+++ controllers/user-controller.js
@@ -0,0 +1,7 @@
+// it is an obje bec . kga h
+// to import indexController.homeController  bec it is an objext
+// so we can directly get by putting {}
+
+module.exports.userController = (req, res) => {
+  res.send(req.randomNumber.toString());
+};
