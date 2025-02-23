cb29fafe9b4432297692f15047f9598451c47896
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-02-23 19:08:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const config = require("config");
+
+// kisi bhi req se phle line
+require("dotenv").config();
+console.log(process.env.EXPRESS_KEYS);
+
+const db = require("./config/mongoose-connection");
+const indexRouter = require("./routes/index-router");
+const userRouter = require("./routes/user-router");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// jb hi hum / router toh hum index router pr pauch jaenge
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+// we can write khud ka config in config folder default file
+app.listen(config.get("PORT"));
+// app.listen(process.env.PORT || 3000);
